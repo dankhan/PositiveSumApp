c801137031d0bf2c500fcf181c959a24df12605f
LeanCTO <dan.khan@gmail.com>
2023-07-14 10:37:36 +1200

    Initial working NS Config
    - working config for tns run android and tns run ios
    - updates app identifiers

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.PositiveSumApp',
+  id: 'ventures.zeropoint.app.positivesum',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
