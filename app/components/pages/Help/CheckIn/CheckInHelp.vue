cb17f5748c3b6a83cef9eea8e128938b3e46eb41
LeanCTO <dan.khan@gmail.com>
2023-07-24 10:00:27 +1200

    Update formatting and text on checkin help page
    - add new message to add people
    - added bold and italic formatting styles

--- app/components/pages/Help/CheckIn/CheckInHelp.vue
+++ app/components/pages/Help/CheckIn/CheckInHelp.vue
@@ -10,9 +10,10 @@
         <!-- Bottom container -->
         <StackLayout dock="bottom" verticalAlignment="bottom" marginLeft="20" marginRight="20" marginBottom="10">
             <Label class="paragraph" :textWrap="true" text="Send a lightweight 'check-in' to let people important to you know that you're thinking about them, and then drop into other contact options if they need more support."></Label>
-            <Label class="paragraph" :textWrap="true" text="Add multiple people into groups to check-in with them all at the same time or to create different check-in contexts."></Label>
+            <Label class="paragraph" :textWrap="true" text="Add multiple people into 'check-in' groups to check-in with them all at the same time or to create different check-in contexts."></Label>
             <Label class="paragraph" :textWrap="true" text="People in your groups only see a 1:1 connection and can reply only to you."></Label>
             <Label class="paragraph" :textWrap="true" text="Building a habit of checking in with others regularly is a great way to create deeper, lasting connections."></Label>
+            <Label class="paragraph bold" :textWrap="true" text="Add one or more people to check-in with to get started!"></Label>
 
             <!-- Got It! button -->
             <Button marginTop="20" class="button-primary" text="Got it!" @tap="onTapBack"></Button>
